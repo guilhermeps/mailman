5668dd070a0113a3cc1a4aac26bacc985cc532dd
Guilherme Silva <guilhermeepsilva@gmail.com>
2018-07-29 17:35:17 -0300

    Gmail quickstart working.

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Mailman
+namespace src
 {
     class Program
     {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Mailman";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            UsersResource.MessagesResource.ListRequest messagesRequest = service.Users.Messages.List("me");
+
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+
+            IList<Message> messages = messagesRequest.Execute().Messages;
+            Console.WriteLine("Messages:");
+            if (messages != null && messages.Count > 0)
+            {
+                foreach (var message in messages)
+                {
+                    Console.WriteLine("{0}", message.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No messages found.");
+            }
+            Console.Read();
         }
     }
 }
