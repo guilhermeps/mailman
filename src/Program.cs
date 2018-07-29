72a9bcdb67d1d554a6e0fa8fca4ad0695447b811
Guilherme Silva <guilhermeepsilva@gmail.com>
2018-07-29 17:02:16 -0300

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mailman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
